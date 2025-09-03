63f44661a783711717e5f3a07f85026aa9895623
oathbreaker <152743670+karansgotit@users.noreply.github.com>
2025-09-02 22:27:17 -0700

    modifying logic to add more features using js

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+const symbolsAlphabets = [ "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const numbersAlphabets = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+const alphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 let firstValue = document.querySelector("#password-1")
 let secondValue = document.querySelector("#password-2")
 let btn = document.querySelector("#main-button")
 let passwordLength = 15
 
-function getrandomPassword(){
+function getrandomPassword(arr){
     let initialString = ""
     for(let i = 0; i < passwordLength;i++){
-        let randomIndex = Math.floor(Math.random()*characters.length)
-        initialString += characters[randomIndex]
+        let randomIndex = Math.floor(Math.random()*arr.length)
+        initialString += arr[randomIndex]
     }
     return initialString
 }
 
-function setValue(){
-    firstValue.value = getrandomPassword()
-    secondValue.value = getrandomPassword() 
+function setValue(password1,password2){
+    firstValue.value = password1
+    secondValue.value = password2
+}
+
+function handleToggle(checkbox){
+    let a = checkbox.id
+    let passwordString =""
+    if(a === "toggle-symbol"){
+        
+    }
 }
